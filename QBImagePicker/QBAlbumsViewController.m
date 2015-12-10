2954db07e35685736a4a49146e7675de27797c31
Cody Kimberling <codykimberling@users.noreply.github.com>
2015-12-09 21:01:44 -0600

    Update QBAlbumsViewController.m

--- QBImagePicker/QBAlbumsViewController.m
+++ QBImagePicker/QBAlbumsViewController.m
@@ -282,6 +282,7 @@ static CGSize CGSizeScale(CGSize size, CGFloat scale) {
     PHAssetCollection *assetCollection = self.assetCollections[indexPath.row];
     
     PHFetchOptions *options = [PHFetchOptions new];
+    options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
     
     switch (self.imagePickerController.mediaType) {
         case QBImagePickerMediaTypeImage:
